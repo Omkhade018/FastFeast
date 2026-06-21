533c46adc74f4bc05fa12a05aa276cc0c8b540de
Om Khade <khadeom238@gmail.com>
2026-06-22 02:43:13 +0530

    Fix Home.js syntax

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -8,23 +8,27 @@ export default function Home() {
   const[foodCat, setFoodCat]= useState([]);
   const [foodItem, setFoodItem] = useState([]);
 
-  const loadData = async ()=>{
-   let response = await fetch(
-  "https://fastfeast-backend.onrender.com/api/foodData",
-  {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }
-);
+  const loadData = async () => {
+  let response = await fetch(
+    "https://fastfeast-backend.onrender.com/api/foodData",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
-response = await response.json();
+  response = await response.json();
 
-    response = await response.json()
+  setFoodItem(response[0]);
+  setFoodCat(response[1]);
 
-    setFoodItem(response[0]);
-    setFoodCat(response[1]);
+
+
+
+
+   
 
     // console.log(response[0],response[1]);
   }
