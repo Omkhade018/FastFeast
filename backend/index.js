c63359ba2743384fe9f92d0fe2fedac6dad8e3c0
Om Khade <khadeom238@gmail.com>
2026-06-22 02:24:45 +0530

    Add CORS methods

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,10 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.setHeader(
+  "Access-Control-Allow-Methods",
+  "GET, POST, PUT, DELETE, OPTIONS"
+);
   next();
 });
 // Routes
